bbc4d8873bbcc0036ec774fefe894bd0e84865e9
Juan Carlos del Rio <jcdre@hotmail.com>
2021-01-06 17:16:42 -0500

    Stripe Checkout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
-import { Jumbo } from "../components"
-import { SEO } from "../components"
-import styled from "styled-components"
+import { graphql } from "gatsby"
+import { Jumbo, SEO, Products } from "../components"
 
 export const query = graphql`
-  query GET_DESCRIPTION {
+  query GET_DATA {
     allSite {
       edges {
         node {
@@ -15,28 +13,35 @@ export const query = graphql`
         }
       }
     }
+    allStripePrice {
+      edges {
+        node {
+          id
+          product {
+            name
+            metadata {
+              img
+              wear
+            }
+          }
+          unit_amount
+        }
+      }
+    }
   }
 `
 
-const Button = styled.button`
-  width: 8rem;
-  background-color: #98ca3f;
-  border: none;
-  border-radius: 10px;
-  color: ${props => props.color};
-  &:hover{
-    transform: scale(1.4)
-  }
-`
-
-const IndexPage = ({ data }) => (
-  <>
-    <SEO title="Home" />
-    <Jumbo description={data.allSite.edges[0].node.siteMetadata.description} />
-    <Button color="white">Comprar</Button>
-    <Link to="/gracias/">Go to gracias</Link>
-    <Link to="/cancel/">Go to Cancel</Link>
-  </>
-)
+const IndexPage = ({ data }) => {
+  console.log(data)
+  return (
+    <>
+      <SEO title="Home" />
+      <Jumbo
+        description={data.allSite.edges[0].node.siteMetadata.description}
+      />
+      <Products products={data.allStripePrice.edges} />
+    </>
+  )
+}
 
 export default IndexPage
